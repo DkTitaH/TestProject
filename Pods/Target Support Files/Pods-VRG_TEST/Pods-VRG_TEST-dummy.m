9df48dc49b22c9bb7964a82374b20f3090751990
Vladymyr Martyniuk <vladymyr.martyniuk@idapgroup.com>
2020-02-01 11:19:42 +0200

    project initial commit

---
+++ Pods/Target Support Files/Pods-VRG_TEST/Pods-VRG_TEST-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VRG_TEST : NSObject
+@end
+@implementation PodsDummy_Pods_VRG_TEST
+@end
---
+++ VRG_TEST/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  VRG_TEST
+//
+//  Created by Vladymyr Martyniuk on 01.02.2020.
+//  Copyright © 2020 Dev. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+//        window.rootViewController =
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
+        return true
+    }
+}
+
